c3ece08648f1868a85a73c04a2071eb03aa3ff85
kathorak <146837122+kathorak@users.noreply.github.com>
2025-01-22 09:50:58 +0100

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,16 +1,20 @@
-# Z následujících si vyber kód a sestav funkční flask aplikaci (není třeba použít vše, vyber si pouze ty části kódu, které potřebuješ)
-# Kód je funkční, pouze místo dotazníků je potřeba doplnit podle potřeby
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__)
 
 
-# app = Flask(__name__)
-# from flask import Flask, render_template, request, redirect, url_for
-# @app.route("???")
-# request.args.get("???")
-# request.form.get("???")
-# print("???")
-# cursor.execute("???")
-# if request.method == "POST"
+@app.route("/")
+def index():
+    return render_template("vitej.html")
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/form")
+def form():
+    recenze = request.args.get("recenze")
+    if recenze == "nic":
+        recenze = "uživatel byl příliš líný na napsání recenze"
+    return render_template("form.html", recenze=recenze)
+
+
+if __name__ == "__main__": #kontrola jestli je app spuštěná přímo a ne jako import
+    app.run(debug=True) #spuštění aplikace v režimu vývojáře
+
